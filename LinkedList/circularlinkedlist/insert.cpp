a3f1186af1f416cb91c3d07a33cbb9f77689411c
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-02-13 16:23:55 +0530

    circular linked list

---
+++ LinkedList/circularlinkedlist/insert.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+//in circular linked list, there is no need of head pointer, only tail pointer is used
+class Node{
+    public:
+    int data;
+     Node* next;
+
+     //constructor
+     Node(int d){
+        this-> data= d;
+        this->next=NULL;
+     }
+
+     ~Node(){
+        int value = this->data;
+        if(this->next!=NULL){
+            delete next;
+            next= NULL;
+        }
+        cout<<"memory is free for node with data "<<value<<endl;
+     }
+};
+
+void insertNode(Node* &tail, int element, int d){
+    //assuming that the element is present in the list
+
+    //empty list
+    if(tail==NULL){
+        Node* newNode= new Node(d);
+        tail = newNode;
+        newNode -> next= newNode;
+    }
+    else{
+        //non empty list
+        Node* curr= tail;
+
+        while (curr->data!=element)
+        {
+            curr= curr-> next;
+        }
+        //element found, curr is representing element wala node
+        Node* temp= new Node(d);
+        temp->next= curr->next;
+        curr->next =temp;
+    }
+}
+void print(Node* &tail){
+    Node* temp = tail;
+    do
+    {
+        cout<<tail->data<<" ";
+        tail=tail->next;
+    } while (tail!=temp);
+    cout<<endl;
+    
+}
+
+int main(){
+    Node* tail = NULL;
+
+    //inseerting in empty list
+    insertNode(tail, 5, 3);
+    print(tail);
+    insertNode(tail, 3, 2);
+    print(tail);
+    insertNode(tail, 2, 5);
+    print(tail);
+    return 0;
+}
