bb23dee6adfb402996159216e19dc6499dc6dfdf
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-02-28 08:15:19 +0530

    recursion question

--- Recursion/CountWays.cpp
+++ Recursion/CountWays.cpp
@@ -2,8 +2,21 @@
 
 #include<iostream>
 using namespace std;
-
+int count(long long nstairs){
+    //base case
+    if(nstairs<0){
+        return 0;
+    }
+    if(nstairs==0){
+        return 1;
+    }
+    int ans = count(nstairs-1) + count(nstairs-2);
+    return ans;
+}
 int main(){
     
+    int n;
+    cin>>n;
+    cout<<count(n)<<endl;
     return 0;
 }
