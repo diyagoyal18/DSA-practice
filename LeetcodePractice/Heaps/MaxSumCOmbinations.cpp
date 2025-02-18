fc59c7803531d3bcdf7747b13d9952757c61f695
Diya Goyal <dgoyal6_be22@thapar.edu>
2025-02-18 22:51:32 +0530

    stack queue

---
+++ LeetcodePractice/Heaps/MaxSumCOmbinations.cpp
@@ -0,0 +1,7 @@
+//InterviewBit Problem: https://www.interviewbit.com/problems/n-max-pair-combinations/
+//Problem Description
+//Given two integers arrays A and B of size N each.
+//Find the maximum N elements from the sum combinations (Ai + Bj) formed from elements in array A and B.
+//Compare this snippet from DSA/LeetcodePractice/Heaps/MaxSumCOmbinations.cpp:
+
+#include <bits/stdc++.h>
